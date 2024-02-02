7b4ea6bc41a10f5326c7fce96e2cf69285e9b96e
thenethi <aravindnethi1632@gmail.com>
2024-02-02 15:34:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+
+const cors=require("cors");
+
+const app = express();
+app.use(cors())
+app.use(express.json())
+
+const dbPath = path.join(__dirname, "blogs.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+// Gives all responses
+app.get("/blogs/", async (request, response) => {
+    const getBlogsArray = `
+    select blogs.id as id,blogs.name as name,blogs.image as image,description.description as description, details.author as author,details.date as date from (blogs inner join description on blogs.id=description.id) as T inner join details on T.id=details.id;`;
+    const blogsArray = await db.all(getBlogsArray);
+    response.send(blogsArray);
+});
